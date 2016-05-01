32dadcec8951be42e36fb25a292ea50f9a1e9cb1
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-05-02 01:56:13 +0300

    Add ability to send messages

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import request
+import telegram
+import os
 app = Flask(__name__)
 
 
@@ -8,7 +10,10 @@ def hello():
     if request.method == "GET":
         return "Not much to see here"
     elif request.method == 'POST':
+        bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN', 'empty_token'))
         user_name = request.json["message"]["chat"]["username"]
+        chat_id = request.json["message"]["chat"]["id"]
+        bot.sendMessage(chat_id, text="Hi! %s" % user_name)
         return "hi %s" % user_name
 
 if __name__ == "__main__":
--- test_server.py
+++ test_server.py
@@ -1,6 +1,7 @@
 import unittest
 from flask.ext.testing import TestCase
 import server
+import mock
 
 
 class TestViews(TestCase):
@@ -15,7 +16,8 @@ class TestViews(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.data, b'Not much to see here')
 
-    def test_empty_post(self):
+    @mock.patch('telegram.Bot')
+    def test_empty_post(self, mock):
         _str_request = '{"message": {"message_id": 12, "text": "aj", "from": {"last_name": "Krivich", "username": "Kiote", "id": 124557099, "first_name": "Ekaterina"}, "chat": {"last_name": "Krivich", "username": "Kiote", "id": 124557099, "type": "private", "first_name": "Ekaterina"}, "date": 1462140570}, "update_id": 627598290}'
         response = self.client.post("/", data=_str_request, content_type='application/json')
         self.assertEqual(response.data, b'hi Kiote')
