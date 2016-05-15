576c03a983e5a3e75eeb88fcb55eddd875afb3e8
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-05-15 13:55:39 +0300

    Add testing env

--- chat/config.py
+++ chat/config.py
@@ -2,7 +2,11 @@ import os
 
 
 travis = os.getenv('BUILD_ON_TRAVIS', False)
+testing = os.getenv('TESTING', False)
+
 if travis:
     DATABASE_URL = 'postgresql://postgres:@127.0.0.1:5432/progressbot_test'
+elif testing:
+    DATABASE_URL = 'postgresql://progressbot_test:123456@localhost:5432/progressbot_test'
 else:
     DATABASE_URL = 'postgresql://progressbot:123456@localhost:5432/progressbot'
