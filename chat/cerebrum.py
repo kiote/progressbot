f6410324342cff5328d161dc53464f6794eeaf0f
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-05-18 18:25:55 +0300

    Add reaction to the new type

--- chat/cerebrum.py
+++ chat/cerebrum.py
@@ -29,13 +29,15 @@ class Cerebrum(object):
     def get_respond(self):
         user_id = self.update['message']['from']['id']
         user_name = self.update['message']['from']['username']
+        message_text = self.update['message']['text'].lower()
         dialog = self.session.query(Dialog).filter_by(user_id=user_id).first()
 
         if dialog is None:
             dialog = Dialog(user_id=user_id, step=self.current_state)
             self.session.add(dialog)
             self.session.commit()
-        else:
-            return "Please enter new type"
+
+        if message_text.startswith('add'):
+            return 'Done, new type had been added.'
 
         return "Hi, %s!" % user_name
